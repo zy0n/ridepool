28d8a0c91cabc0b9b18ba893636d4a2a9dee269c
Huggy Bear <Huggy Bear>
2017-01-18 19:59:05 -0600

    Fixxed

---
+++ app/secrets_tmp.py
@@ -0,0 +1,44 @@
+# Copy this file into secrets.py and set keys, secrets and scopes.
+
+# Details of your community
+community = {
+	'name': 'Tanglewood Forest',
+	'address': 'Austin, TX',
+	'lat': 30.184655,
+	'lng': -97.833937
+}
+
+# This is a session secret key used by webapp2 framework.
+# Get 'a random and long string' from here:
+# http://clsc.net/tools/random-string-generator.php
+# or execute this from a python shell: import os; os.urandom(64)
+SESSION_KEY = c2xbj60d10l9gd76ts7pfm12wj8q432f
+
+# Google APIs
+GOOGLE_APP_ID = 628022227548-8lt6hfs818rm8uiktbr25662v425kds4.apps.googleusercontent.com
+GOOGLE_APP_SECRET = a1QDIl-67qbWjrpFtW1eDRb1
+
+# Facebook auth apis
+FACEBOOK_APP_ID =
+FACEBOOK_APP_SECRET =
+
+# https://dev.twitter.com/apps
+TWITTER_CONSUMER_KEY =
+TWITTER_CONSUMER_SECRET =
+
+
+# config that summarizes the above
+# do not modify the following
+AUTH_CONFIG = {
+  # OAuth 2.0 providers
+  'google'      : (GOOGLE_APP_ID, GOOGLE_APP_SECRET,
+                  'https://www.googleapis.com/auth/userinfo.profile'),
+
+  'facebook'    : (FACEBOOK_APP_ID, FACEBOOK_APP_SECRET,
+                  'user_about_me'),
+
+  # OAuth 1.0 providers don't have scopes
+  'twitter'     : (TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET)
+
+  # OpenID doesn't need any key/secret
+}
